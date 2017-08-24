92965a1b2c5f5d0bb0a2639857f64aeaefd15a93
Chad Estioco <chadestioco@gmail.com>
2017-08-25 01:31:56 +0800

    This callback shit works.

--- scripts/jcvd.ts
+++ scripts/jcvd.ts
@@ -1,25 +1,33 @@
 /// <reference path="../node_modules/@types/chrome/chrome-app.d.ts" />
+/// <reference path="../node_modules/@types/chrome/index.d.ts" />
 
-function checkHN(): boolean{
-    var windowLocation: string;
-    var semIsUrlQueried: boolean = false
-    var spam = chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs){
-        windowLocation = tabs[0].url;
-        console.log("inside", windowLocation);
-        return true;
-    });
-    console.log("outside", windowLocation);
-    console.log("spam", spam);
-    return windowLocation == "news.ycombinator.com";
+/**
+Expects fullUrl to look like "https://domain.com/possibleArgs".
+*/
+function getDomain(fullUrl: string): string{
+    var urlParse: string[] = fullUrl.split("/");
+    return urlParse[2];
 }
 
-function main(){
+function _getUrl(callback){
+    chrome.tabs.query({"active": true, "lastFocusedWindow": true}, (tabs: chrome.tabs.Tab[]) => {
+        var windowLocation: string = getDomain(tabs[0].url);
+        console.log("_getUrl url is", windowLocation);
+        callback(windowLocation == "news.ycombinator.com");
+    });
+}
+
+function pseudoMain(isHN: boolean){
     var det = document.getElementById("det");
-    if(checkHN()){
+    if(isHN){
         det.innerHTML = "At HN";
     } else{
         det.innerHTML = "Not at HN";
     }
+}
+
+function main(){
+    _getUrl(pseudoMain);
 }
 
 window.onload = main;
