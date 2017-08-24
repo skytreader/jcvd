877936dbe08feb3da4a93ee2495dbc6874ac206b
Chad Estioco <chadestioco@gmail.com>
2017-08-25 00:20:57 +0800

    I can get the proper URL now but I've no idea how to manage the asyncness of this.

--- scripts/jcvd.ts
+++ scripts/jcvd.ts
@@ -1,10 +1,19 @@
-function checkHN(){
-    console.log(window.location.href);
-    return window.location.href == "news.ycombinator.com";
+/// <reference path="../node_modules/@types/chrome/chrome-app.d.ts" />
+
+function checkHN(): boolean{
+    var windowLocation: string;
+    var semIsUrlQueried: boolean = false
+    var spam = chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs){
+        windowLocation = tabs[0].url;
+        console.log("inside", windowLocation);
+        return true;
+    });
+    console.log("outside", windowLocation);
+    console.log("spam", spam);
+    return windowLocation == "news.ycombinator.com";
 }
 
 function main(){
-    console.log("main called");
     var det = document.getElementById("det");
     if(checkHN()){
         det.innerHTML = "At HN";
@@ -12,3 +21,5 @@ function main(){
         det.innerHTML = "Not at HN";
     }
 }
+
+window.onload = main;
