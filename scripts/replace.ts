6cc784645fac45d8391edfec332ecfcca5890c68
Chad Estioco <chadestioco@gmail.com>
2017-08-25 06:47:42 +0800

    Works as a proof-of-concept but is obviously bloated because jquery.

---
+++ scripts/replace.ts
@@ -0,0 +1,6 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts" />
+console.log("replace loaded");
+$(document).ready(() => {
+    //$(this).attr("href", "http://skytreader.net");
+    $("a").attr("href", "http://skytreader.net");
+});
