b3aa77ac49d03b71cb1b4794f4d51c2af9f0a252
Chad Estioco <chadestioco@gmail.com>
2017-09-19 23:00:30 +0800

    At last this works haha.

--- compile_watcher.py
+++ compile_watcher.py
@@ -26,8 +26,8 @@ class CompileEventHandler(FileSystemEventHandler):
     def __compile(self, src):
         outfile = self.__to_js(src)
         subprocess.call([
-            #"node_modules/typescript/bin/tsc", "--lib", "es2015,es2015.iterable", "--outFile", outfile, src
-            "node_modules/typescript/bin/tsc", "--lib", "es6", "--outFile", outfile, src
+            "node_modules/typescript/bin/tsc", "--lib", "es2015,es2015.iterable,dom", "--outFile", outfile, src
+            #"node_modules/typescript/bin/tsc", "--lib", "es6", "--outFile", outfile, src
         ])
         logging.info("compiled %s to %s" % (src, outfile))
     
