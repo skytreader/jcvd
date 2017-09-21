25f2ad86794cac1abb97c0a21d7fe6f5f2be17b9
Chad Estioco <chadestioco@gmail.com>
2017-09-21 16:43:56 +0800

    Savepoint.

--- scripts/replace.ts
+++ scripts/replace.ts
@@ -1,12 +1,16 @@
 /// <reference path="../node_modules/@types/chrome/chrome-app.d.ts" />
 /// <reference path="../node_modules/@types/jquery/index.d.ts" />
 
+console.log("test");
+
 $(document).ready(() => {
-    //$(this).attr("href", "http://skytreader.net");
+    $(this).attr("href", "http://skytreader.net");
     //$("a").attr("href", "test.html");
-    $("a").click(() => {
-        chrome.tabs.create({"url": chrome.extension.getURL("test.html")},
-            (tab) => {}
-        );
-    });
+    console.log($(this));
+    //$("a").click(() => {
+    //    console.log("click detected");
+    //    chrome.tabs.create({"url": chrome.extension.getURL("test.html")},
+    //        (tab) => {}
+    //    );
+    //});
 });
