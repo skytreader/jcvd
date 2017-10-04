d2018dd4fe10d319ed52900fc6ce21b393dd4c85
Chad Estioco <chadestioco@gmail.com>
2017-10-04 18:23:34 +0800

    Savepoint of desperation.

--- scripts/replace.ts
+++ scripts/replace.ts
@@ -4,9 +4,12 @@
 console.log("test");
 
 $(document).ready(() => {
+    console.log($(this).find($(".storylink")));
     $(this).attr("href", "http://skytreader.net");
     //$("a").attr("href", "test.html");
     console.log($(this));
+    console.log("href click jQuery storylink");
+    console.log($(this).attr("href"));
     //$("a").click(() => {
     //    console.log("click detected");
     //    chrome.tabs.create({"url": chrome.extension.getURL("test.html")},
