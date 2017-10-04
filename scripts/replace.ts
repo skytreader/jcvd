99f35ad55d0ce3ef3816bda1febe6fb0c8b042e0
Chad Estioco <chadestioco@gmail.com>
2017-10-04 20:27:51 +0800

    Add debugging note.

--- scripts/replace.ts
+++ scripts/replace.ts
@@ -4,16 +4,16 @@
 console.log("test");
 
 $(document).ready(() => {
-    console.log($(this).find($(".storylink")));
-    $(this).attr("href", "http://skytreader.net");
+    //console.log($(this).find($(".storylink")));
+    //$(this).attr("href", "http://skytreader.net");
     //$("a").attr("href", "test.html");
-    console.log($(this));
-    console.log("href click jQuery storylink");
-    console.log($(this).attr("href"));
-    //$("a").click(() => {
-    //    console.log("click detected");
-    //    chrome.tabs.create({"url": chrome.extension.getURL("test.html")},
-    //        (tab) => {}
-    //    );
-    //});
+    //console.log($(this));
+    //console.log("href click jQuery storylink");
+    //console.log($(this).attr("href"));
+    $("a").click(() => {
+        console.log("click detected");
+        chrome.tabs.create({"url": chrome.extension.getURL("test.html")},
+            (tab) => {}
+        );
+    });
 });
